2572462a5abbeffa6ab6eea6d428ba5ed80af0e8
Joo <vijak13019@hotmail.com>
2022-12-24 11:47:28 +0700

    refactor(app)

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,90 @@
-import './App.css';
-import InputField from './components/InputField';
-import TodoList from './components/TodoList';
-import { useState, useReducer, useEffect } from 'react';
-import { todoReducer } from './components/todoReducer';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-import { api } from './utils/axios';
+import './App.css'
+
+import { useEffect, useReducer, useState } from 'react'
+import { DragDropContext, DropResult } from 'react-beautiful-dnd'
+
+import InputField from './components/InputField'
+import TodoList from './components/TodoList'
+import { todoReducer } from './components/todoReducer'
+import { api } from './utils/axios'
 
 const App: React.FC = () => {
-  const [todoText, setTodoText] = useState<string>('');
-  const [todos, dispatchTodo] = useReducer(todoReducer, []);
-  const [toRemoves, dispatchToRemove] = useReducer(todoReducer, []);
+  const [todoText, setTodoText] = useState<string>('')
+  const [todos, dispatchTodo] = useReducer(todoReducer, [])
+  const [toRemoves, dispatchToRemove] = useReducer(todoReducer, [])
 
   useEffect(() => {
     async function initializeData() {
       try {
-        const res = await api.get('/todos');
-        console.log(res.data);
+        const res = await api.get('/todos')
+        console.log(res.data)
         dispatchTodo({
           type: 'initialize',
           todos: res.data.todos,
-        });
+        })
         dispatchToRemove({
           type: 'initialize',
           todos: res.data.toRemoves,
-        });
+        })
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     }
-    initializeData();
-  }, []);
+    initializeData()
+  }, [])
 
   const handleAdd = (event: React.FormEvent) => {
-    event.preventDefault();
+    event.preventDefault()
 
     dispatchTodo({
       type: 'add',
       id: Date.now(),
       todoText,
       isDone: false,
-    });
+    })
 
-    setTodoText('');
-  };
+    setTodoText('')
+  }
 
   const onDragEnd = (result: DropResult) => {
     // console.log(result)
-    const { source, destination } = result;
+    const { source, destination } = result
 
-    if (!destination) return;
+    if (!destination) return
     if (
       source.droppableId === destination.droppableId &&
       source.index === destination.index
     )
-      return;
+      return
 
-    let item;
+    let item
     if (source.droppableId === 'TodosList') {
-      item = todos[source.index];
+      item = todos[source.index]
       dispatchTodo({
         type: 'delete',
         id: item.id,
-      });
+      })
     } else {
-      item = toRemoves[source.index];
+      item = toRemoves[source.index]
       dispatchToRemove({
         type: 'delete',
         id: item.id,
-      });
+      })
     }
 
-    console.log(item);
+    console.log(item)
     if (destination.droppableId === 'TodosList') {
       dispatchTodo({
         type: 'add',
         ...item,
-      });
+      })
     } else {
       dispatchToRemove({
         type: 'add',
         ...item,
-      });
+      })
     }
-  };
+  }
 
   return (
     <>
@@ -103,7 +105,7 @@ const App: React.FC = () => {
         </main>
       </DragDropContext>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
